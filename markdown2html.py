90e20ee377751155b5ee5fb9ceedf9a625456a34
mandinou <113857342+Amandine4731@users.noreply.github.com>
2023-12-15 10:57:57 +0100

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -5,38 +5,27 @@
 '''
 
 
-import sys
-import os
-import markdown2
-
 def convert_markdown_to_html(input_file, output_file):
-    # Check if the Markdown file exists
     if not os.path.exists(input_file):
         sys.stderr.write(f"Missing {input_file}\n")
         sys.exit(1)
 
-    # Read the Markdown content
     with open(input_file, 'r', encoding='utf-8') as md_file:
         markdown_content = md_file.read()
 
-    # Convert Markdown to HTML
     html_content = markdown2.markdown(markdown_content)
 
-    # Write the HTML content to the output file
     with open(output_file, 'w', encoding='utf-8') as html_file:
         html_file.write(html_content)
 
 if __name__ == "__main__":
-    # Check the number of command-line arguments
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py <input_file> <output_file>\n")
         sys.exit(1)
 
-    # Get input and output file names from command-line arguments
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    # Convert Markdown to HTML and handle errors
     try:
         convert_markdown_to_html(input_file, output_file)
         sys.exit(0)
