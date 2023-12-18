18c8613218c09618cabbc361057a687adc7ec7eb
mandinou <113857342+Amandine4731@users.noreply.github.com>
2023-12-18 10:00:25 +0100

    chmod

--- markdown2html.py
+++ markdown2html.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Write a script markdown2html.py that takes an argument 2 strings:
    First argument is the name of the Markdown file.
-   Second argument is the output file name.
+   Second argument is the output file name
 '''
 
 
@@ -12,10 +12,9 @@ import os
 if __name__ == '__main__':
     
     if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
-        sys.exit(1)
+        sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
+        exit(1)
 
     if not os.path.isfile(sys.argv[1]):
-        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
-        sys.exit(1)
+        sys.stderr.write('Missing {}'.format(sys.argv[1]))
+        exit(1)
