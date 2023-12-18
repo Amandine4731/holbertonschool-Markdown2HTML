dd4e95a913a9dc2daa1718a51a40c6024e5c20b4
mandinou <113857342+Amandine4731@users.noreply.github.com>
2023-12-18 10:22:38 +0100

    test config permissions

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Write a script markdown2html.py that takes an argument 2 strings:
+   First argument is the name of the Markdown file.
+   Second argument is the output file name
+'''
+
+
+import sys
+import os
+
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) < 3:
+        sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
+        exit(1)
+
+    if not os.path.isfile(sys.argv[1]):
+        sys.stderr.write('Missing {}\n'.format(sys.argv[1]))
+        exit(1)
